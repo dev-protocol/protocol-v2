5bd28f42ea4693220f7da8fb2e5c9f7ab2d98ec3
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-09-24 15:36:16 +0900

    update dev token

---
+++ contracts/interface/IArbToken.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MPL-2.0
+// solhint-disable-next-line compiler-version
+pragma solidity ^0.8.0;
+
+// see
+// https://github.com/OffchainLabs/arbitrum/blob/master/packages/arb-bridge-peripherals/contracts/tokenbridge/arbitrum/IArbToken.sol
+// It was written in version 0.6.11, so it was created to support 0.8.
+
+interface IArbToken {
+    /**
+     * @notice should increase token supply by amount, and should (probably) only be callable by the L1 bridge.
+     */
+    function bridgeMint(address account, uint256 amount) external;
+
+    /**
+     * @notice should decrease token supply by amount, and should (probably) only be callable by the L1 bridge.
+     */
+    function bridgeBurn(address account, uint256 amount) external;
+
+    /**
+     * @return address of layer 1 token
+     */
+    function l1Address() external view returns (address);
+}
--- contracts/src/dev/Dev.sol
+++ contracts/src/dev/Dev.sol
@@ -5,6 +5,7 @@ import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/
 import {AccessControlEnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgradeable.sol";
 import {InitializableUsingRegistry} from "contracts/src/common/registry/InitializableUsingRegistry.sol";
 import {IDev} from "contracts/interface/IDev.sol";
+import {IArbToken} from "contracts/interface/IArbToken.sol";
 
 /**
  * The contract used as the DEV token.
@@ -16,23 +17,41 @@ import {IDev} from "contracts/interface/IDev.sol";
 contract Dev is
 	ERC20Upgradeable,
 	AccessControlEnumerableUpgradeable,
+	IArbToken,
 	InitializableUsingRegistry,
 	IDev
 {
+	address public override l1Address;
 	bytes32 public constant override BURNER_ROLE = keccak256("BURNER_ROLE");
 	bytes32 public constant override MINTER_ROLE = keccak256("MINTER_ROLE");
-
 	/**
 	 * Initialize the passed address as AddressRegistry address.
 	 * The token name is `Dev`, the token symbol is `DEV`, and the decimals is 18.
 	 */
-	function initializeDev(address _registry) external initializer {
+	function initialize(address _registry, address _l1DevAddress) external initializer {
 		__ERC20_init("Dev", "DEV");
 		__AccessControlEnumerable_init();
 		__UsingRegistry_init(_registry);
 		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 		_setupRole(BURNER_ROLE, _msgSender());
 		_setupRole(MINTER_ROLE, _msgSender());
+		l1Address = _l1DevAddress;
+	}
+
+	function bridgeMint(address account, uint256 amount) external override {
+		this.mint(account, amount);
+	}
+
+	function bridgeBurn(address account, uint256 amount) external override {
+		this.burn(account, amount);
+	}
+
+	function mint(address _account, uint256 _amount) external override {
+		require(
+			hasRole(MINTER_ROLE, _msgSender()),
+			"must have minter role to mint"
+		);
+		_mint(_account, _amount);
 	}
 
 	function burn(address _account, uint256 _amount) external override {
@@ -41,13 +60,5 @@ contract Dev is
 			"must have burner role to burn"
 		);
 		_burn(_account, _amount);
-	}
-
-	function mint(address _account, uint256 _amount) public override {
-		require(
-			hasRole(MINTER_ROLE, _msgSender()),
-			"must have minter role to mint"
-		);
-		_mint(_account, _amount);
 	}
 }
