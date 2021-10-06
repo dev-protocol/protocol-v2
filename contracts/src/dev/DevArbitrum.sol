5ef459e841ade2a55c7c23e446f730b486c28c2b
aggre <hiroyuki.aggre@gmail.com>
2021-10-06 23:38:36 +0900

    add todo comment to remember the requirement

--- contracts/src/dev/DevArbitrum.sol
+++ contracts/src/dev/DevArbitrum.sol
@@ -11,6 +11,9 @@ import {Dev} from "contracts/src/dev/Dev.sol";
  * When authenticated a new asset by the Market contracts, DEV token is burned as fees.
  */
 contract DevArbitrum is Dev {
+	// TODO
+	// `l1Address` getter should be returned an address of a deployed https://github.com/dev-protocol/dev-arb-one/blob/main/contracts/Dev.sol
+
 	/**
 	 * Initialize the passed address as AddressRegistry address.
 	 * The token name is `Dev`, the token symbol is `DEV`, and the decimals is 18.
