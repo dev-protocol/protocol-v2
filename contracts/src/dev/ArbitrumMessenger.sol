f84a66b200f70e949d521f8e89c24c76ffa29bb8
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-10-08 16:01:00 +0900

    udpate by lint

--- contracts/src/dev/ArbitrumMessenger.sol
+++ contracts/src/dev/ArbitrumMessenger.sol
@@ -3,26 +3,23 @@ pragma solidity =0.8.9;
 import {IArbSys} from "contracts/interface/IArbSys.sol";
 
 abstract contract ArbitrumMessenger {
-    address internal constant DDD = address(100);
+	address internal constant DDD = address(100);
 
-    event TxToL1(
-        address indexed _from,
-        address indexed _to,
-        uint256 indexed _id,
-        bytes _data
-    );
+	event TxToL1(
+		address indexed _from,
+		address indexed _to,
+		uint256 indexed _id,
+		bytes _data
+	);
 
-    function sendTxToL1(
-        uint256 _l1CallValue,
-        address _from,
-        address _to,
-        bytes memory _data
-    ) internal virtual returns (uint256) {
-        uint256 _id = IArbSys(DDD).sendTxToL1{value: _l1CallValue}(
-            _to,
-            _data
-        );
-        emit TxToL1(_from, _to, _id, _data);
-        return _id;
-    }
+	function sendTxToL1(
+		uint256 _l1CallValue,
+		address _from,
+		address _to,
+		bytes memory _data
+	) internal virtual returns (uint256) {
+		uint256 _id = IArbSys(DDD).sendTxToL1{value: _l1CallValue}(_to, _data);
+		emit TxToL1(_from, _to, _id, _data);
+		return _id;
+	}
 }
