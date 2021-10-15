d81697588fee0b0b9c6274708ab3d0db0934482e
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-10-15 16:02:09 +0900

    update by lint

--- migrations/update/1_update-metrics-factory.ts
+++ migrations/update/1_update-metrics-factory.ts
@@ -11,14 +11,18 @@ const handler = async function (deployer, network) {
 	const logicInstance = await logic.deployed()
 	console.log(`logic address:${logicInstance.address}`)
 
-	const adminInstance = await artifacts.require('DevAdmin').at('0x00551f424BD3426A1B15eb1Ea4680cc2bf7E9D76')
+	const adminInstance = await artifacts
+		.require('DevAdmin')
+		.at('0x00551f424BD3426A1B15eb1Ea4680cc2bf7E9D76')
 	console.log(`admin address:${adminInstance.address}`)
 
 	const metricsFactoryProxy = '0x650663aD898A018cca44Ac224Be2286D14B7421d'
 
 	await adminInstance.upgrade(metricsFactoryProxy, logicInstance.address)
 
-	const implAddress = await adminInstance.getProxyImplementation(metricsFactoryProxy)
+	const implAddress = await adminInstance.getProxyImplementation(
+		metricsFactoryProxy
+	)
 
 	console.log(`impl address:${implAddress}`)
 } as Truffle.Migration
