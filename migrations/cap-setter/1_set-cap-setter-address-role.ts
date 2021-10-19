74c25066e9504e8d838b85b645d57ab5ab259685
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-10-19 12:35:44 +0900

    update by lint

--- migrations/cap-setter/1_set-cap-setter-address-role.ts
+++ migrations/cap-setter/1_set-cap-setter-address-role.ts
@@ -15,7 +15,9 @@ const handler = async function (deployer, network) {
 		.require('AddressRegistry')
 		.at(registryProxy)
 	console.log(`registry proxy address:${addressRegistryInstance.address}`)
-	const capSetterAddressBefore = await addressRegistryInstance.registries('CapSetter')
+	const capSetterAddressBefore = await addressRegistryInstance.registries(
+		'CapSetter'
+	)
 	console.log(`cap setter address:${capSetterAddressBefore}`)
 	await addressRegistryInstance.setRegistry('CapSetter', capSetter)
 	const capSetterAddress = await addressRegistryInstance.registries('CapSetter')
