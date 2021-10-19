155f30a23c3786ec2cd0494906753b00f2705268
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-10-19 12:34:26 +0900

    add script

---
+++ migrations/cap-setter/1_set-cap-setter-address-role.ts
@@ -0,0 +1,25 @@
+
+/* eslint-disable @typescript-eslint/await-thenable */
+
+const handler = async function (deployer, network) {
+	if (network === 'test') {
+		return
+	}
+
+	// Check it!!!//////////////////////////////////////////////////////
+	const registryProxy = ''
+	const capSetter = ''
+	/// ////////////////////////////////////////////////////////////////
+
+	const addressRegistryInstance = await artifacts
+		.require('AddressRegistry')
+		.at(registryProxy)
+	console.log(`registry proxy address:${addressRegistryInstance.address}`)
+	const capSetterAddressBefore = await addressRegistryInstance.registries('CapSetter')
+	console.log(`cap setter address:${capSetterAddressBefore}`)
+	await addressRegistryInstance.setRegistry('CapSetter', capSetter)
+	const capSetterAddress = await addressRegistryInstance.registries('CapSetter')
+	console.log(`cap setter address:${capSetterAddress}`)
+} as Truffle.Migration
+
+export = handler
