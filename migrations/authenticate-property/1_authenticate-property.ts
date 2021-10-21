8b16cf12feff51bf7bbea9cd6bbfaddc6232ae1e
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-10-21 10:52:36 +0900

    add script

---
+++ migrations/authenticate-property/1_authenticate-property.ts
@@ -0,0 +1,23 @@
+
+/* eslint-disable @typescript-eslint/await-thenable */
+
+const handler = async function (deployer, network) {
+	if (network === 'test') {
+		return
+	}
+
+	// Check it!!!//////////////////////////////////////////////////////
+	const marketAddress = ''
+	const propertyAddress = ''
+	const arg1 = ''
+	const arg2= ''
+	/// ////////////////////////////////////////////////////////////////
+
+	const marketInstance = await artifacts
+		.require('Market')
+		.at(marketAddress)
+	console.log(`market address:${marketInstance.address}`)
+	await marketInstance.authenticate(propertyAddress, [arg1, arg2])
+} as Truffle.Migration
+
+export = handler
