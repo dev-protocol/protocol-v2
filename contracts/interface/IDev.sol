c990b98c61d2aec3021c4c77e8f418040cc13268
0xkenta <kenta0558kiichi614@gmail.com>
2021-11-10 16:17:13 +0100

    delete MINTER_ROLE and BURNER_ROLE

--- contracts/interface/IDev.sol
+++ contracts/interface/IDev.sol
@@ -3,12 +3,6 @@
 pragma solidity ^0.8.0;
 
 interface IDev {
-	// solhint-disable-next-line func-name-mixedcase
-	function MINTER_ROLE() external returns (bytes32);
-
-	// solhint-disable-next-line func-name-mixedcase
-	function BURNER_ROLE() external returns (bytes32);
-
 	function mint(address _account, uint256 _amount) external;
 
 	function burn(address _account, uint256 _amount) external;
