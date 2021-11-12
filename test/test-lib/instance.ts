5e0ed33a46a20045e118997ca8e53371e688fff7
0xkenta <kenta0558kiichi614@gmail.com>
2021-11-12 16:33:26 +0100

    add para addMinterRole and if sentence to add MinterRole into generateLockup()

--- test/test-lib/instance.ts
+++ test/test-lib/instance.ts
@@ -187,7 +187,7 @@ export class DevProtocolInstance {
 		)
 	}
 
-	public async generateLockup(): Promise<void> {
+	public async generateLockup(addMinterRole=true): Promise<void> {
 		const [proxfied] = await deployProxy(contract('Lockup'), this._deployer)
 		await proxfied.initialize(this._addressRegistry.address)
 		this._lockup = proxfied
@@ -196,7 +196,9 @@ export class DevProtocolInstance {
 			this._lockup.address,
 			this.fromDeployer
 		)
-		await this._dev.grantRole(await this._dev.MINTER_ROLE(), proxfied.address)
+		if (addMinterRole) {
+            await this._dev.grantRole(await this._dev.MINTER_ROLE(), proxfied.address)
+		}
 	}
 
 	public async generatePropertyFactory(): Promise<void> {
