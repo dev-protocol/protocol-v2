35b776fa3274d45462ef5461df13b504748f7321
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-11-17 12:19:17 +0900

    update deploy scripot

---
+++ migrations/update/1_redeploy-market-factory.ts
@@ -0,0 +1,51 @@
+const handler = async function (deployer, network) {
+	if (network === 'test') {
+		return
+	}
+
+	// Check it!!!//////////////////////////////////////////////////////
+	const marketAddress1 = ''
+	const marketAddress2 = ''
+	const adminAddress = ''
+	const addressRegistry = ''
+	/// ////////////////////////////////////////////////////////////////
+
+	const logic = artifacts.require('MarketFactory')
+	await deployer.deploy(logic)
+	const logicInstance = await logic.deployed()
+	console.log(`logic address:${logicInstance.address}`)
+
+	const adminInstance = await artifacts.require('DevAdmin').at(adminAddress)
+	console.log(`admin address:${adminInstance.address}`)
+
+	const devProxy = artifacts.require('DevProxy')
+	await deployer.deploy(
+		devProxy,
+		logicInstance.address,
+		adminInstance.address,
+		web3.utils.fromUtf8('')
+	)
+	const proxyInstance = await devProxy.deployed()
+	console.log(`proxy address:${proxyInstance.address}`)
+
+	const regInstance = await artifacts
+		.require('AddressRegistry')
+		.at(addressRegistry)
+	console.log(`registry address:${regInstance.address}`)
+
+	await regInstance.setRegistry('MarketFactory', proxyInstance.address)
+	console.log('set proxy address to registry')
+
+	const setAddress = await regInstance.registries('MarketFactory')
+	console.log(`set proxy address is ${setAddress}`)
+
+	const wrap = await logic.at(proxyInstance.address)
+	await wrap.initialize(regInstance.address)
+
+	await wrap.__addMarketAddress(marketAddress1)
+	await wrap.__addMarketAddress(marketAddress2)
+	const addresses = await wrap.getEnabledMarkets()
+	console.log(addresses)
+} as Truffle.Migration
+
+export = handler
