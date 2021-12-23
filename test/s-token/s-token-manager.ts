b29f09ba1b428af5b061f6f98d0836edd9d95aee
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2021-12-23 16:14:06 +0900

    bug fix

--- test/s-token/s-token-manager.ts
+++ test/s-token/s-token-manager.ts
@@ -1,4 +1,5 @@
 /* eslint-disable max-params */
+import { encode } from 'js-base64'
 import BigNumber from 'bignumber.js'
 import { DevProtocolInstance } from '../test-lib/instance'
 import { PropertyInstance } from '../../types/truffle-contracts'
@@ -106,7 +107,8 @@ contract('STokensManager', ([deployer, user]) => {
 		expect(imageInfo.length).to.equal(2)
 		expect(imageInfo[0]).to.equal('data:image/svg+xml;base64')
 		const testImage = `<svg xmlns="http://www.w3.org/2000/svg" width="290" height="500" viewBox="0 0 290 500" fill="none"><rect width="290" height="500" fill="url(#paint0_linear)"/><path fill-rule="evenodd" clip-rule="evenodd" d="M192 203H168.5V226.5V250H145H121.5V226.5V203H98H74.5V226.5V250V273.5H51V297H74.5H98V273.5H121.5H145H168.5H192V250V226.5H215.5H239V203H215.5H192Z" fill="white"/><text fill="white" xml:space="preserve" style="white-space: pre" font-family="monospace" font-size="11" letter-spacing="0em"><tspan x="27.4072" y="333.418">${property}</tspan></text><defs><linearGradient id="paint0_linear" x1="0" y1="0" x2="290" y2="500" gradientUnits="userSpaceOnUse"><stop stop-color="#00D0FD"/><stop offset="0.151042" stop-color="#4889F5"/><stop offset="0.552083" stop-color="#D500E6"/><stop offset="1" stop-color="#FF3815"/></linearGradient></defs></svg>`
-		expect(imageInfo[1]).to.equal(testImage)
+		const encoded = encode(testImage)
+		expect(imageInfo[1]).to.equal(encoded)
 	}
 
 	const checkTokenImageUri = (image: string, tokenUriImage: string): void => {
