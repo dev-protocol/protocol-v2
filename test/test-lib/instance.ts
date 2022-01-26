cfc9cb40e26bcc6e68ab08cd767acad6d0542169
0xkenta <kenta0558kiichi614@gmail.com>
2022-01-26 10:53:32 +0900

    modify by lint

--- test/test-lib/instance.ts
+++ test/test-lib/instance.ts
@@ -30,8 +30,8 @@ export const deployProxy = async <L extends ContractInstance>(
 	deployer: string
 ): Promise<[ReturnType<L['at']>, DevAdminInstance]> => {
 	const admin = await contract('DevAdmin').new()
-	const impl = await logic.new() as { address: string }
-	
+	const impl = (await logic.new()) as { address: string }
+
 	const proxy = await contract('DevProxy').new(
 		impl.address,
 		admin.address,
