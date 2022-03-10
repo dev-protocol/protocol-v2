c0343b7397127bffa21917e31dc9ec9a0e2c0a0a
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2022-03-10 10:10:30 +0900

    update by lint

--- migrations/grant-roll/1_grant-roll-dev-polygon.ts
+++ migrations/grant-roll/1_grant-roll-dev-polygon.ts
@@ -5,7 +5,9 @@ const handler = async function (deployer, network) {
 		return
 	}
 
-	const devPolygonInstance = await artifacts.require('DevPolygon').at(process.env.DEV_POLYGON!)
+	const devPolygonInstance = await artifacts
+		.require('DevPolygon')
+		.at(process.env.DEV_POLYGON!)
 	console.log(`dev polygon address:${devPolygonInstance.address}`)
 
 	const minterRole = await devPolygonInstance.MINTER_ROLE()
@@ -18,9 +20,15 @@ const handler = async function (deployer, network) {
 	await devPolygonInstance.grantRole(bunerRole, devBridgeAddress)
 	await devPolygonInstance.grantRole(minterRole, devBridgeAddress)
 
-	const hasBurnerRoll = await devPolygonInstance.hasRole(bunerRole, devBridgeAddress)
+	const hasBurnerRoll = await devPolygonInstance.hasRole(
+		bunerRole,
+		devBridgeAddress
+	)
 	console.log(`burner roll:${hasBurnerRoll}`)
-	const hasMinterRoll = await devPolygonInstance.hasRole(minterRole, devBridgeAddress)
+	const hasMinterRoll = await devPolygonInstance.hasRole(
+		minterRole,
+		devBridgeAddress
+	)
 	console.log(`minter roll:${hasMinterRoll}`)
 } as Truffle.Migration
 
