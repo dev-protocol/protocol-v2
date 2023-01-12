d327bfca364a2f391e695dff691fd3fd2c623a44
aggre <hiroyuki.aggre@gmail.com>
2023-01-12 12:46:05 +0900

    Using @openzeppelin/truffle-upgrades (#547)
    using @openzeppelin/truffle-upgrades

--- migrations/update/1_update-lockup.ts
+++ migrations/update/1_update-lockup.ts
@@ -1,24 +1,27 @@
-const handler = async function (deployer, network) {
+import { upgradeProxy } from '@openzeppelin/truffle-upgrades'
+import { type ContractClass } from '@openzeppelin/truffle-upgrades/dist/utils'
+
+const handler = async function (_, network) {
 	if (network === 'test') {
 		return
 	}
 
 	const adminAddress = process.env.ADMIN!
 	const proxyAddress = process.env.LOCKUP_PROXY!
-	console.log(`admin address:${adminAddress}`)
-	console.log(`lockup proxy address:${proxyAddress}`)
+	console.log('Admin address:', adminAddress)
+	console.log('Lockup proxy address:', proxyAddress)
 
-	const logic = artifacts.require('Lockup')
-	await deployer.deploy(logic)
-	const logicInstance = await logic.deployed()
-	console.log(`logic address:${logicInstance.address}`)
-
-	const adminInstance = await artifacts.require('DevAdmin').at(adminAddress)
-	await adminInstance.upgrade(proxyAddress, logicInstance.address)
-
-	const implAddress = await adminInstance.getProxyImplementation(proxyAddress)
-
-	console.log(`impl address:${implAddress}`)
+	await upgradeProxy(
+		proxyAddress,
+		artifacts.require('Lockup') as unknown as ContractClass
+	)
+	console.log(
+		'New implementation:',
+		await artifacts
+			.require('DevAdmin')
+			.at(adminAddress)
+			.getProxyImplementation(proxyAddress)
+	)
 } as Truffle.Migration
 
 export = handler
